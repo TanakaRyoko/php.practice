9ccd6562bd2c1bdf2272f6b8c44f12328a1008df
TANAKA RYOKO <ryochi0825@yahoo.co.jp>
2019-09-29 09:56:55 +0000

    　追加

--- practice.php
+++ practice.php
@@ -12,3 +12,25 @@ $hello="Hello,";
 $name="ryoko";
 $world="s world!";
 echo $hello. $name. $world ;
+
+$tech_boost="tech";
+echo $tech_boost."boost";
+
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+// 12月を表示する
+echo $calendar_2018["December"];
+
